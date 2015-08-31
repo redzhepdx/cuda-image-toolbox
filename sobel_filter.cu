4ab7300a943e694bc600eeaacbd569577737be0f
Tolga Ay <tolgaay@live.com>
2015-08-31 15:26:45 +0300

    Polished the Sobel Filter
    Added error checking.
    Added memory cleaning.
    Turned the main task into a function.

--- sobel_filter.cu
+++ sobel_filter.cu
@@ -8,7 +8,7 @@
 
 using namespace cv;
 
-// Kernel function
+// Sobel Filter Kernel function
 __global__ void sobel_filter(unsigned char* d_img, unsigned char* d_out, const int ROWS, const int COLS) {
     int idx = threadIdx.x + blockIdx.x*blockDim.x;
     int idy = threadIdx.y + blockIdx.y*blockDim.y;
@@ -30,6 +30,59 @@ __global__ void sobel_filter(unsigned char* d_img, unsigned char* d_out, const i
     }
 }
 
+// Apply sobel filter function
+int applySobelFilter(Mat* in_img, Mat* out_img){
+    // Create an empty image with same dimensions
+    const int ROWS = in_img->rows;
+    const int COLS = in_img->cols;
+
+    // Copy the image info to an unsigned char array
+    unsigned char* h_img = (unsigned char*)malloc(ROWS*COLS*sizeof(unsigned char));
+    if(h_img == NULL){
+        printf("ERROR: malloc - Couldn't allocate memory for the image channel input, on the host.");
+        return -1;
+    }
+    for(int i = 0; i < ROWS; i++)
+        for(int j = 0; j < COLS; j++)
+            h_img[i*COLS + j] = image.at<uchar>(i, j);
+
+    // Transfer the channel to the device
+    unsigned char *d_img;
+    checkCudaErrors(cudaMalloc((void**)&d_img, ROWS*COLS*sizeof(unsigned char)));
+    checkCudaErrors(cudaMemcpy(d_img, h_img, ROWS*COLS*sizeof(unsigned char), cudaMemcpyHostToDevice));
+
+    // Determine the block and grid sizes
+    dim3 blockSize(4, 4, 1);
+    dim3 gridSize((int)((COLS - 1) / blockSize.x) + 1, (int)((ROWS - 1) / blockSize.y) + 1, 1);
+
+    // Allocate memory on device for the image output
+    unsigned char *d_out;
+    checkCudaErrors(cudaMalloc((void**)&d_out, ROWS*COLS*sizeof(unsigned char)));
+
+    // Run the Sobel filter on the image
+    sobel_filter<<<gridSize, blockSize>>>(d_img, d_out, ROWS, COLS);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    // Transfer the channel back to host
+    unsigned char* h_out = (unsigned char*)malloc(ROWS*COLS*sizeof(unsigned char));
+    if(h_out == NULL){
+        printf("ERROR: malloc - Couldn't allocate memory for the image channel output, on the host.");
+        return -1;
+    }
+    checkCudaErrors(cudaMemcpy(h_out, d_out, ROWS*COLS*sizeof(unsigned char), cudaMemcpyDeviceToHost));
+
+    // Save the channel to the Mat
+    for(int i = 0; i < ROWS; i++)
+        for(int j = 0; j < COLS; j++)
+            out_img->at<uchar>(i, j) = h_out[i*COLS + j];
+
+    // Cleanup
+    if(h_img != NULL) free(h_img);
+    if(h_out != NULL) free(h_out);
+    checkCudaErrors(cudaFree(d_img));
+    checkCudaErrors(cudaFree(d_out));
+    return 0;
+}
 
 // Main function
 int main() {
@@ -45,42 +98,16 @@ int main() {
     waitKey(0);
 
     // Create an empty image with same dimensions
-    const int ROWS = in_img.rows;
-    const int COLS = in_img.cols;
-    Mat out_img = Mat::zeros(ROWS, COLS, CV_8UC1);
+    Mat out_img = Mat::zeros(in_img.rows, in_img.cols, CV_8UC1);
 
-    // Copy the image info to an unsigned char array
-    unsigned char* h_img = (unsigned char*)malloc(ROWS*COLS*sizeof(unsigned char));
-    for(int i = 0; i < ROWS; i++)
-        for(int j = 0; j < COLS; j++)
-            h_img[i*COLS + j] = image.at<uchar>(i, j);
-
-    // Transfer the channel to the device
-    unsigned char *d_img;
-    cudaMalloc((void**)&d_img, ROWS*COLS*sizeof(unsigned char));
-    cudaMemcpy(d_img, h_img, ROWS*COLS*sizeof(unsigned char), cudaMemcpyHostToDevice);
-
-    // Determine the block and grid sizes
-    dim3 blockSize(4, 4, 1);
-    dim3 gridSize((int)((COLS - 1) / blockSize.x) + 1, (int)((ROWS - 1) / blockSize.y) + 1, 1);
-
-    // Run the Sobel filter on the image
-    unsigned char *d_out;
-    cudaMalloc((void**)&d_out, ROWS*COLS*sizeof(unsigned char));
-    sobel_filter<<<gridSize, blockSize>>>(d_img, d_out, ROWS, COLS);
-
-    // Transfer the channel back to host
-    unsigned char* h_out = (unsigned char*)malloc(ROWS*COLS*sizeof(unsigned char));
-    cudaMemcpy(h_out, d_out, ROWS*COLS*sizeof(unsigned char), cudaMemcpyDeviceToHost);
-
-    // Save the channel to the Mat
-    for(int i = 0; i < ROWS; i++)
-        for(int j = 0; j < COLS; j++)
-            out_img.at<uchar>(i, j) = h_out[i*COLS + j];
+    // Apply the filter
+    int result = applySobelFilter(&in_img, &out_img);
 
     // Display the output image
-    imshow("Output Image", out_img);
-    waitKey(0);
+    if(result == 0){
+        imshow("Output Image", out_img);
+        waitKey(0);
+    }
 
-    return 0;
+    return result;
 }
